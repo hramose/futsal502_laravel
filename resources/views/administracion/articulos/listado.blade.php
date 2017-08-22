e0289d461593ab2448fadac26259d19cc71f0894
Alejandro Muralles <alejandromuralles57@gmail.com>
2017-08-21 19:58:33 -0600

    cambios

--- resources/views/administracion/articulos/listado.blade.php
+++ resources/views/administracion/articulos/listado.blade.php
@@ -15,8 +15,8 @@
 						<th>TITULO</th>
 						<th>AUTOR</th>
 						<th>FECHA PUBLICACION</th>
-						<th>PUBLICO</th>
 						<th>PUBLICADO</th>
+						<th>VISTAS</th>
 						<th></th>
 					</tr>
 				</thead>
@@ -24,10 +24,10 @@
 					@foreach($articulos as $articulo)
 					<tr>
 						<td>{{$articulo->titulo}}</td>
-						<td>{{$articulo->autor->nombre}}</td>
+						<td>{{$articulo->autor->username}}</td>
 						<td>{{$articulo->fecha_publicacion}}</td>
-						<td>{!! $articulo->descripcion_publico !!}</td>
 						<td>{!! $articulo->descripcion_publicado !!}</td>
+						<td>{{ $articulo->vistas }}</td>
 						<td>
 							<a href="{{route('editar_articulo',$articulo->id)}}" class="btn btn-warning btn-sm btn-flat fa fa-edit" data-toggle="tooltip" data-placement="top" title="" data-original-title="Editar"></a>
 							<a href="{{route('medias_articulos',$articulo->id)}}" class="btn btn-primary btn-sm btn-flat fa fa-image" data-toggle="tooltip" data-placement="top" title="" data-original-title="Medias"></a>
