dbc36b91fc90c187a7703e18ded7d07c3543e97c
Alejandro Muralles <alejandromuralles57@gmail.com>
2018-01-26 07:11:43 -0600

    estilos en calendario

--- resources/views/externo/calendario.blade.php
+++ resources/views/externo/calendario.blade.php
@@ -31,9 +31,11 @@
 										<td class="text-center" style="color: white !important; background-color: #063e71" width="20%">
 											<!--<a href="{{route('ficha',$partido->id)}}" class="text-white" style="text-decoration: none; font-weight: bold" data-toggle="tooltip" data-placement="top" title="" data-original-title="Ver Ficha">-->
 												@if($partido->estado != 'P')
-													{{$partido->goles_local}} - {{$partido->goles_visita}}
+													<span style="font-size: 20px; font-weight: bold">
+														{{$partido->goles_local}} - {{$partido->goles_visita}}
+													</span>
 												@else
-													<span style="font-size: 10px">
+													<span style="font-size: 12px">
 													{{date('d-m',strtotime($partido->fecha))}} /
 													{{date('H:i',strtotime($partido->fecha))}}
 													</span>
