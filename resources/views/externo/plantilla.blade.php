b248a6fa9e45c69b05476f9ac4c63b25a52ef37b
Alejandro Muralles <alejandromuralles57@gmail.com>
2018-03-01 07:41:44 -0600

    plantillas

--- resources/views/externo/plantilla.blade.php
+++ resources/views/externo/plantilla.blade.php
@@ -19,62 +19,39 @@
 						</div>
 					</div>
 					<div class="row">
-						<h3><span><i class="red-color">CUERPO </i>TÉCNICO</span></h3>
-						@foreach($cuerpoTecnico as $ct)
-						<div class="col-lg-4 col-sm-4 col-xs-6 r-full-width">
-							<div class="team-column">
-								<img src="{{$ct->persona->fotografia}}" alt="">
-								<div class="team-detail">
-									<h5><a href="#">{{$ct->persona->nombre_completo}}</a></h5>
-									<span class="desination">{{$ct->descripcion_posicion}}</span>
-									<div class="detail-inner">
-										<ul>
-											<li>Pais</li>
-											<li>Posición</li>
-											<li>Sexo</li>
-											<li></li>
-										</ul>
-										<ul>
-											<li>{{$ct->persona->pais->descripcion}}</li>
-											<li>{{$ct->descripcion_posicion}}</li>
-											<li>{{$ct->persona->descripcion_genero}}</li>
-											<li></li>
-										</ul>
-									</div>
-								</div>
-							</div>
+						<div class="table-responsive">
+							<table class="table table-responsive">
+								<thead>
+									<tr>
+										<td>DORSAL</td>
+										<td>NOMBRE</td>
+										<td>POSICION</td>
+									</tr>
+								</thead>
+								<tbody>
+									@foreach($cuerpoTecnico as $ct)
+									<tr>
+										<td></td>
+										<td>
+											<img src="{{$ct->persona->fotografia}}" alt="">
+											{{$ct->persona->nombre_completo}}
+										</td>
+										<td>{{$ct->descripcion_posicion}}</td>
+									</tr>
+									@endforeach
+									@foreach($jugadores as $j)
+									<tr>
+										<td>{{$j->dorsal}}</td>
+										<td>
+											<img src="{{$j->persona->fotografia}}" alt="">
+											{{$j->persona->nombre_completo}}
+										</td>
+										<td>{{$j->descripcion_posicion}}</td>
+									</tr>
+									@endforeach
+								</tbody>
+							</table>
 						</div>
-						@endforeach
-					</div>
-					<hr>
-					<div class="row">
-						<h3><span><i class="red-color">JUGADORES </i></span></h3>
-						@foreach($jugadores as $j)
-						<div class="col-lg-4 col-sm-4 col-xs-6 r-full-width">
-							<div class="team-column">
-								<img src="{{$j->persona->fotografia}}" alt="">
-								<span class="player-number">{{$j->dorsal}}</span>
-								<div class="team-detail">
-									<h5><a href="#">{{$j->persona->nombre_completo}}</a></h5>
-									<span class="desination">{{$j->descripcion_posicion}}</span>
-									<div class="detail-inner">
-										<ul>
-											<li>Pais</li>
-											<li>Posición</li>
-											<li>Dorsal</li>
-											<li>Sexo</li>
-										</ul>
-										<ul>
-											<li>{{$j->persona->pais->descripcion}}</li>
-											<li>{{$j->descripcion_posicion}}</li>
-											<li>{{$j->dorsal}}</li>
-											<li>{{$j->persona->descripcion_genero}}</li>
-										</ul>
-									</div>
-								</div>
-							</div>
-						</div>
-						@endforeach
 					</div>
 				</div>
 			</div>
